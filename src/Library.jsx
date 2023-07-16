da1ee80ff65d335e71e7a97591c84e461850cbdf
Jordi Mont <jordi@milimetricmkt.com>
2023-07-16 20:11:56 +0200

    Improve code with parent stats. Remove children stats For better handle

--- src/Library.jsx
+++ src/Library.jsx
@@ -3,33 +3,64 @@ import { Book } from './components/Book.jsx'
 
 export function Library() {
     const [books, setBooks] = useState([]);
-    const [totalBooks, setTotalBooks] = useState(0);
-    const [readingBooks, setReadingBooks] = useState(0);
+    const [readingBooks, setReadingBooks] = useState([])
 
-    const handleIsReadingChange = (isReading) => {
-        setReadingBooks(prevReadingBooks => isReading ? prevReadingBooks + 1 : prevReadingBooks - 1);
-    };
-
+    // Hacemos la petición a la API cuando inicia el componente
     useEffect(() => {
         fetch('https://raw.githubusercontent.com/midudev/pruebas-tecnicas/main/pruebas/01-reading-list/books.json')
             .then(response => response.json())
             .then(data => {
                 setBooks(data.library);
-                setTotalBooks(data.library.length);
             })
             .catch(error => console.error(error));
     }, []);
 
+    // Manejamos el estado de los libros que estamos leyendo
+    const handleReadingBooks = (ISBN) => {
+
+        // Hacemos una copia del array de libros que estamos leyendo
+        const newReadingBooks = [...readingBooks]
+
+        // Aquí comprobamos si el libro que estamos leyendo está en el array
+        const index = newReadingBooks.indexOf(ISBN)
+        if (index === -1) {
+
+            // Añadimos el libro al array de libros que estamos leyendo
+            newReadingBooks.push(ISBN)
+            setReadingBooks(newReadingBooks)
+        } else {
+
+            // Eliminamos el libro del array de libros que estamos leyendo
+            newReadingBooks.splice(index, 1)
+            setReadingBooks(newReadingBooks)
+        }
+    };
+
     return (
         <div className='library bg-light-bg p-20 rounded-[40px] shadow-[0_0_60px_60px_rgba(0,0,0,0.1)]'>
-            <h1 className="total-nooks font-serif text-accent text-4xl font-bold mb-2">Número de libros: {totalBooks}</h1>
-            <h2 className="counter__wrapper text-xl font-bold mb-6">Libros que estamos leyendo: {readingBooks}</h2>
-            <div className="books relative z-10 grid grid-cols-4 gap-12">
+            <div className='flex w-full items-center justify-between'>
+                <div className='titles'>
+                    <h1 className='total-nooks font-serif text-accent text-4xl font-bold mb-2'>Número de libros: {books.length}</h1>
+                    <h2 className='counter__wrapper text-xl font-bold mb-6'>Libros que estamos leyendo: {readingBooks.length}</h2>
+                </div>
+                {/* <div className='reset'>
+                    <button className='border border-accent2 p-3 rounded text-accent2'>Reset</button>
+                </div> */}
+            </div>
+            <div className='books relative z-10 grid grid-cols-4 gap-12'>
                 {books.map((bookWrapper, index) => {
-                    const book = bookWrapper.book; // Aquí extraemos el objeto book
+                    const book = bookWrapper.book // Aquí extraemos el objeto book
                     return (
-                        <Book key={index} genre={book.genre} img={book.cover} title={book.title}  handleIsReadingChange={handleIsReadingChange} />
-                    );
+                        <Book
+                            key={index}
+                            ISBN={book.ISBN}
+                            genre={book.genre}
+                            img={book.cover}
+                            title={book.title}
+                            handleReadingBooks={handleReadingBooks}
+                            isReading={readingBooks.includes(book.ISBN)}
+                        />
+                    )
                 })}
             </div>
         </div>
--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -2,35 +2,32 @@ import React, { useEffect, useState } from 'react'
 import { Plus } from './Plus.jsx'
 import { Tick } from './Tick.jsx'
 
-export function Book({ img, title, genre, handleIsReadingChange }) {
-	const [isReading, setIsReading] = useState(false)
+export function Book({ img, title, ISBN, genre, handleReadingBooks, isReading, book }) {
 
-	const handleClick = () => {
-		const newIsReadingState = !isReading;
-		setIsReading(newIsReadingState);
-		handleIsReadingChange(newIsReadingState);
-	}
-
-	const reading = isReading ? 'book group is-reading' : 'book group';
+	const className = `book group ${isReading ? 'is-reading' : ''}`
 	const readingText = isReading ? <Tick /> : <Plus />;
 
 	const imageStyle = {
 		backgroundImage: `url(${img})`,
 	}
 
+	const handleClick = () => {
+		handleReadingBooks(ISBN)
+	}
+
 	return (
-		<div className={reading}>
-			<div className="book__wrapper relative cursor-pointer" onClick={handleClick}>
-				<div className="hidden group-hover:flex group-[.is-reading]:flex absolute top-2 left-2 z-10 w-[30px] h-[30px] items-center justify-center group-hover:bg-bg group-[.is-reading]:bg-accent rounded">{readingText}</div>
-				<div className="book__cover relative">
-					<div class="book-cover" style={imageStyle}>
-						<div class="effect"></div>
-						<div class="light"></div>
+		<div className={className}>
+			<div className='book__wrapper relative cursor-pointer' onClick={handleClick}>
+				<div className='hidden group-hover:flex group-[.is-reading]:flex absolute top-2 left-2 z-10 w-[30px] h-[30px] items-center justify-center group-hover:bg-bg group-[.is-reading]:bg-accent rounded'>{readingText}</div>
+				<div className='book__cover relative'>
+					<div className='book-cover' style={imageStyle}>
+						<div className='effect'></div>
+						<div className='light'></div>
 					</div>
-					<div class="book-inside">
+					<div className='book-inside'>
 					</div>
 				</div>
-				<div className="book__info">
+				<div className='book__info'>
 					<p className='font-serif text-center mt-2 text-xl font-bold'>{title}</p>
 					<p className='text-center italic'>{genre}</p>
 				</div>
